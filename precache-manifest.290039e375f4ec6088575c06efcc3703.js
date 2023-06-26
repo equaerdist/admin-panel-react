3dfef99d052c10831bf0c170ade91edcb6ccd640
equaer <vriyazzz1001@gmail.com>
2023-06-26 23:01:51 +0300

    final

---
+++ precache-manifest.290039e375f4ec6088575c06efcc3703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d15aab980ac77200386",
+    "url": "/static/css/main.d2cf8eb3.chunk.css"
+  },
+  {
+    "revision": "9d15aab980ac77200386",
+    "url": "/static/js/main.9d15aab9.chunk.js"
+  },
+  {
+    "revision": "5bb86d37328d0408c46b",
+    "url": "/static/css/1.9a4a2a75.chunk.css"
+  },
+  {
+    "revision": "5bb86d37328d0408c46b",
+    "url": "/static/js/1.5bb86d37.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bdaba72689745e709a221fdd40b30014",
+    "url": "/index.html"
+  }
+];
